44ec018daab22df4fdec5fcbe670a4c45f6ec580
AislynnEdmiston <davidandaislynn@gmail.com>
2019-08-12 23:25:51 -0400

    created my elements

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,13 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new artible
 
 */
+
+function articleCreator (article) {
+  const div = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const p1 = document.createElement('p');
+  const p2 = document.createElement('p');
+  const p3 = document.createElement('p');
+  const span = document.createElement('span');
+
+}
